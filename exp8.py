d4e28e7afaae1c0fd707222f43fb1500eb724757
11249a033-dot <11249a033@kanchiuniv.ac.in>
2026-09-10 15:09:12 +0530

    Add files via upload

---
+++ exp8.py
@@ -0,0 +1,12 @@
+import spacy
+nlp = spacy.load("en_core_web_sm")
+text = input("Enter a sentence: ")
+doc = nlp(text)
+
+doc = nlp(text)
+print("\nNamed Entities")
+print("-" * 40)
+for ent in doc.ents:
+    print(f"Entity : {ent.text}")
+    print(f"Lable : {ent.label_}")
+    print("-" * 40)
