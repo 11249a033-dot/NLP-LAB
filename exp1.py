059bd5ce02528abcb89c03e97ce1673840dd8688
11249a033-dot <11249a033@kanchiuniv.ac.in>
2026-09-10 15:07:08 +0530

    Add files via upload

---
+++ exp1.py
@@ -0,0 +1,5 @@
+from nltk.tokenize import sent_tokenize, word_tokenize
+example_text = "Hello Mr.smith,How are you doing today?The weather is great and python is awesome..."
+print(word_tokenize(example_text))
+print(sent_tokenize(example_text))
+
